49920666d7e030acc58e9b226815d59ea6b010db
Matt Lyell <mlyell21@Gmail.com>
2022-07-11 02:10:18 -0700

    practice problems

--- RB101/practice_problems/med_2/lettecase_percentage.rb
+++ RB101/practice_problems/med_2/lettecase_percentage.rb
@@ -9,6 +9,7 @@
 
 # Algorithm
 
+p '1'.upcase(:ascii)
 
 letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
 letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
